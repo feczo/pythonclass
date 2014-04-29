36706db573203a4640999405193d685922c778df
feczo <szabolcs.feczak@gmail.com>
2014-04-29 13:31:00 +1000

    Update truthcheck.py

--- truthcheck.py
+++ truthcheck.py
@@ -2,6 +2,6 @@ days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
         'Friday', 'Saturday', 'Sunday']
 
 for day in days:
-    for hour in range(1, 24):
+    for hour in range(24):
         if not(day == 'Monday' or hour == 12):
             print "%s %d" % (day, hour)
